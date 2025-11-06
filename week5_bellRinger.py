a2417ded7ba9c70a3ec91a1f84b5c90d17062043
iboylan <pipiel326@gmail.com>
2025-11-06 18:40:33 +0000

    AHHHHHHHHHHHHHHHHHHHHHHHHH

--- week5_bellRinger.py
+++ week5_bellRinger.py
@@ -15,17 +15,24 @@ print(magic.rindex('a'))
 # Given the string 
 alphabet = 'abcdefghijklmnopqrstuvwxyz'
 # a. Extract the letters 'hij'.
+print(alphabet[alphabet.index('h'):alphabet.index('k')])
 # b. Extract every second letter starting from 'a' to 'm'.
+print(alphabet[0:13:2])
 # c. Reverse the entire string using slicing.
 print(alphabet[::-1])
+print(alphabet[::-5])
 
 # Problem Set 2: Extracting Information
 # From Descriptions:
 # Extract the name of the famous personality from the quote "Ask not what your country can do for you — ask what you can do for your country. - John F. Kennedy"
-
+dream = 'When we allow freedom to ring—when we let it ring from every city and every hamlet, from every state and every city, we will be able to speed up that day when all of God’s children, black men and white men, Jews and Gentiles, Protestants and Catholics, will be able to join hands and sing in the words of the old Negro spiritual, “Free at last, Free at last, Great God a-mighty, We are free at last.”'
+print(dream)
+print(dream[::-1])
 # Manipulating Words:
-# Given the string info = "Python is fun. Fun is good. Good is subjective.",
+# Given the string 
+info = "Python is fun. Fun is good. Good is subjective."
 # a. Extract the word 'subjective' without knowing its exact position.
+print(info[info.rindex('s'):info.rindex('e')+1])
 # b. Extract every third word.
 # c. Reverse the positions of the words, but keep the characters in each word in the same order.
 
